1ecd38d32a092ff9f86696158e7a603a1b2806a8
Paweł Gacek <pawelgacek.pl@gmail.com>
2024-09-01 15:05:44 +0200

    GEM-168 | add UseUserDetails.tsx && UseProfilePicture.tsx

---
+++ hooks/attachment/UseProfilePicture.tsx
@@ -0,0 +1,41 @@
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
+import { useContext } from 'react';
+
+import { ImageBase64 } from '@/components/ui/image/CustomImage';
+import { API_URL, APPLICATION_JSON_INTERNAL_VER_1, HOST, PATHS } from '@/constants/Api';
+import { GlobalContext } from '@/context/GlobalContext';
+
+export default function useProfilePicture(attachmentId?: string) {
+  const { authState } = useContext(GlobalContext);
+  return useQuery({
+    queryKey: [`/users/${authState.userId}/attachments/${attachmentId}`],
+    queryFn: async (): Promise<ImageBase64> => {
+      const { data } = await axios.get(
+        `${API_URL}${PATHS.EXTERNAL}/users/${authState.userId}/attachments/${attachmentId}`,
+        {
+          headers: {
+            host: HOST.ATTACHMENT_STORE,
+            accept: APPLICATION_JSON_INTERNAL_VER_1,
+            authorization: `Bearer ${authState.token}`,
+          },
+          responseType: 'blob',
+        },
+      );
+      const reader = new FileReader();
+      const blobPromise = new Promise<string>((resolve, reject) => {
+        reader.onloadend = () => resolve(reader.result as string);
+        reader.onerror = reject;
+        reader.readAsDataURL(data);
+      });
+
+      const base64Data = await blobPromise;
+
+      return {
+        uri: base64Data,
+      };
+    },
+    refetchOnMount: false,
+    enabled: !!attachmentId,
+  });
+}
