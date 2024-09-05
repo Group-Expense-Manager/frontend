58fca391c70f74bd93e1a1a3012b25ca6c51a10f
Paweł Gacek <pawelgacek.pl@gmail.com>
2024-09-05 19:12:31 +0200

    GEM-172 | fix translation

--- app/(auth)/(register-stepper)/register-username.tsx
+++ app/(auth)/(register-stepper)/register-username.tsx
@@ -26,7 +26,7 @@ export default function RegisterUsername() {
       rule(arg: string) {
         return arg.length <= 20;
       },
-      errorMessage: t('Username must contain at most 20 characters'),
+      errorMessage: t('Username may contain at most 20 characters'),
     },
     {
       rule: /^[a-zA-Z0-9_.+-]+$/,
