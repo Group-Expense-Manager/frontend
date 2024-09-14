9b582d97bc0880e48bb6694ec088cf96eb769d15
Paweł Gacek <pawelgacek.pl@gmail.com>
2024-09-14 12:13:09 +0200

    GEM-187 | removed unused GroupContext

--- app/groups/_layout.tsx
+++ app/groups/_layout.tsx
@@ -2,18 +2,14 @@ import { Stack } from 'expo-router';
 import React from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
-import { GroupProvider } from '@/context/group/GroupContext';
-
 export default function GroupsLayout() {
   return (
-    <GroupProvider>
-      <SafeAreaView className="flex-1">
-        <Stack
-          screenOptions={{
-            headerShown: false,
-          }}
-        />
-      </SafeAreaView>
-    </GroupProvider>
+    <SafeAreaView className="flex-1">
+      <Stack
+        screenOptions={{
+          headerShown: false,
+        }}
+      />
+    </SafeAreaView>
   );
 }
